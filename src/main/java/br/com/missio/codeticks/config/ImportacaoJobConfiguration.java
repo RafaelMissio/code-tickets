742becc9de1c577b1a3ee930c7b0edece443ccdd
Rafael Missio <rtmissio@gmail.com>
2026-09-13 12:49:24 -0300

    add step initial configuration

--- src/main/java/br/com/missio/codeticks/config/ImportacaoJobConfiguration.java
+++ src/main/java/br/com/missio/codeticks/config/ImportacaoJobConfiguration.java
@@ -1,17 +1,26 @@
 package br.com.missio.codeticks.config;
 
+import br.com.missio.codeticks.entities.Importacao;
 import org.springframework.batch.core.job.Job;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.job.parameters.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.Step;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.infrastructure.item.ItemReader;
+import org.springframework.batch.infrastructure.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class ImportacaoJobConfiguration {
 
+    private final PlatformTransactionManager transactionManager;
+
+    public ImportacaoJobConfiguration(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
 
     @Bean
     public Job job(JobRepository jobRepository, Step stepInicial) {
@@ -20,6 +29,20 @@ public class ImportacaoJobConfiguration {
                 .incrementer(new RunIdIncrementer())
                 .build();
     }
+
+
+    @Bean
+    public Step stepInicial(JobRepository jobRepository,
+                            ItemReader<Importacao> reader,
+                            ItemWriter<Importacao> writer){
+        return new StepBuilder("stepInicial", jobRepository)
+                .<Importacao, Importacao>chunk(200)
+                .transactionManager(transactionManager)
+                .reader(reader)
+                .writer(writer)
+                .build();
+    }
+
 
 
 }
