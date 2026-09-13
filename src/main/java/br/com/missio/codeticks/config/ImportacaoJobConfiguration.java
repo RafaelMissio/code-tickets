e73df4060017c6110143da89bd965cead7b01682
Rafael Missio <rtmissio@gmail.com>
2026-09-13 13:10:56 -0300

    add reader configuration

--- src/main/java/br/com/missio/codeticks/config/ImportacaoJobConfiguration.java
+++ src/main/java/br/com/missio/codeticks/config/ImportacaoJobConfiguration.java
@@ -9,8 +9,11 @@ import org.springframework.batch.core.step.Step;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.infrastructure.item.ItemReader;
 import org.springframework.batch.infrastructure.item.ItemWriter;
+import org.springframework.batch.infrastructure.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
@@ -42,6 +45,21 @@ public class ImportacaoJobConfiguration {
                 .writer(writer)
                 .build();
     }
+
+    @Bean
+    public ItemReader<Importacao> reader(@Value("${IMPORTACAO_CSV_PATH}")
+                                             String importacaoCsvPath) {
+        return new FlatFileItemReaderBuilder<Importacao>()
+                .name("leitura-csv")
+                .resource(new FileSystemResource(importacaoCsvPath))
+                .comments("--")
+                .delimited()
+                .names("cpf", "cliente", "nascimento", "evento", "data", "tipoIngresso", "valor")
+                .targetType(Importacao.class)
+                .build();
+    }
+
+
 
 
 
