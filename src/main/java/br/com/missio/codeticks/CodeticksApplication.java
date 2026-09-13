fce9c6a11ae2dfd6f0c2d2e5627a1811b5d20b7c
Rafael Missio <rtmissio@gmail.com>
2026-09-13 12:33:13 -0300

    add Importacao entity

---
+++ src/main/java/br/com/missio/codeticks/CodeticksApplication.java
@@ -0,0 +1,13 @@
+package br.com.missio.codeticks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeticksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeticksApplication.class, args);
+    }
+
+}
